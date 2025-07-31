9c50e73347cfcf6ed8131e85e6113fc21596f0d2
Your Name <41466655+Sajaalghoul@users.noreply.github.com>
2025-07-31 12:34:45 +0200

    google auth

--- src/HighOrderComponents/withGoogleAuth/withGoogleAuth.jsx
+++ src/HighOrderComponents/withGoogleAuth/withGoogleAuth.jsx
@@ -9,24 +9,23 @@ const withGoogleAuth = (Component) => {
       const [isLoggedin, setIsLoggedin] = useState(false);
     
       const handleLogin = () => {
-        const callbackUrl = `${window.location.origin}`;
+        const callbackUrl = `https://sajaalghoul.github.io/BookStore/`;
         const googleClientId = "9009554555-nvd2uuq6jb3giptk195j6vt6g55krbfp.apps.googleusercontent.com";
         const targetUrl = `https://accounts.google.com/o/oauth2/auth?redirect_uri=${encodeURIComponent(
           callbackUrl
         )}&response_type=token&client_id=${googleClientId}&scope=openid%20email%20profile%20https://www.googleapis.com/auth/books&prompt=select_account`;
           window.location.href = targetUrl;
         };
-    
-        useEffect(() => {
-          const accessTokenRegex = /access_token=([^&]+)/;
-          const isMatch = window.location.href.match(accessTokenRegex);
-    
-          if (isMatch) {
-            const accessToken = isMatch[1];
-            Cookies.set("access_token", accessToken);
-            setIsLoggedin(true);
-          }
-        }, []);
+      useEffect(() => {
+        const accessTokenRegex = /access_token=([^&]+)/;
+        const isMatch = window.location.href.match(accessTokenRegex);
+        if (isMatch) {
+          const accessToken = isMatch[1];
+          Cookies.set("access_token", accessToken);
+          window.history.replaceState(null, '', window.location.pathname);
+          setIsLoggedin(true);
+        }
+      }, []);
     
         useEffect(() => {
           if (isLoggedin) {
